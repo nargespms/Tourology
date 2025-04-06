e034b3826fa2b56cb589f677549c036108697317
Narges Pourmoghadasi <nargespms.np@gmail.com>
2025-04-05 18:16:13 -0600

    fix: update SOCKET_URL to match the new online socket address

--- Client/app/api/config.ts
+++ Client/app/api/config.ts
@@ -1,3 +1,2 @@
 export const API_URL = "http://49.13.114.192:4000"; // Online API
-// export const API_URL = "http://172.16.1.67:4000"; // home setup
-export const SOCKET_URL = "http://172.16.1.159:4000"; // Online Socket
+export const SOCKET_URL = "http://49.13.114.192:4000"; // Online Socket
